3388e22bd836e2fbdf79c7dca44c5d3c8762e85e
Awan <95389195+Awan69@users.noreply.github.com>
2023-05-17 11:27:56 +0700

    Update sederhana.py

--- Stack/sederhana.py
+++ Stack/sederhana.py
@@ -1,1 +1,12 @@
+stack = [1,2,3,4,5]
+
+stack.append (6)
+print(f"Tumoukan Masuk {6}")
+
+stack.append(7)
+print(f"Tumpukan Masuk {7}")
+
+out = stack.pop()
+print(f"Tumpukan Keluar {out}")
+print(f"Tumpukan Sekarang {stack}")
 
