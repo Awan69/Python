740fc65671e4562bf5016e74e2f2070814984010
Awan <95389195+Awan69@users.noreply.github.com>
2023-05-17 11:25:18 +0700

    Add files via upload

--- Queue/sederhana.py
+++ Queue/sederhana.py
@@ -1,6 +1,11 @@
-from collections import deque
+stack = [1,2,3,4,5]
 
-queue = deque ([1,2,3,4,5])
+stack.append (6)
+print(f"Tumoukan Masuk {6}")
 
-queue.append(6)
-print()
+stack.append(7)
+print(f"Tumpukan Masuk {7}")
+
+out = stack.pop()
+print(f"Tumpukan Keluar {out}")
+print(f"Tumpukan Sekarang {stack}")
